14328721dc55411dcd8e3c56e15db0b0d40e4853
Egor Volvachev <e.volvachev@cti.ru>
2018-02-21 10:53:08 +0300

    unsubscribe

--- src/app/feature/list/list.component.ts
+++ src/app/feature/list/list.component.ts
@@ -1,22 +1,33 @@
 import { ApiService } from '@feature/services/api.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subject } from 'rxjs/Subject';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss']
 })
-export class ListComponent implements OnInit {
+export class ListComponent implements OnInit, OnDestroy {
 
   public lists: IList[] = [];
+
+  private destroyStream = new Subject<void>();
 
   constructor(private readonly apiService: ApiService) { }
 
   public ngOnInit(): void {
     this.apiService.get<IList[]>('assets/list.mock.json')
+      .pipe(
+        takeUntil(this.destroyStream)
+      )
       .subscribe(item => {
         this.lists = item;
       });
+  }
+
+  public ngOnDestroy(): void {
+    this.destroyStream.next();
   }
 
   public sort(fieldName: string): void {
